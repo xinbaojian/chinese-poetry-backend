e7d782e76ae22b5de3517b63b55594b5505c3720
xinbaojian <baojian.xin.py@gmail.com>
2026-05-15 00:27:16 +0800

    ✨ feat(db): 扩展 mastery_level 枚举为 new/learning/reviewing/mastered

---
+++ migrations/007_update_mastery_level_enum.sql
@@ -0,0 +1,8 @@
+-- Update mastery_level ENUM from ('proficient','fair','weak') to ('new','learning','reviewing','mastered')
+-- MySQL ALTER ENUM requires re-specifying the full column definition
+
+ALTER TABLE learning_records
+    MODIFY COLUMN mastery_level ENUM('new', 'learning', 'reviewing', 'mastered') NOT NULL DEFAULT 'new';
+
+ALTER TABLE review_history
+    MODIFY COLUMN mastery_level ENUM('new', 'learning', 'reviewing', 'mastered') NOT NULL;
